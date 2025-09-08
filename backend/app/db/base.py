39ae3db07eff7627e972edb296b99cbbca30ea6a
Clara Battesini <clarabattesini@gmail.com>
2025-09-07 22:55:10 -0400

    fix circular dependency

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -1,3 +1,4 @@
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+# Import all the models, so that Base has them before being
+# imported by Alembic
+from app.db.base_class import Base
+from app.models.user import User
